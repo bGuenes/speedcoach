ed9d8325969747a20af7ea8bda43029d02204575
bGuenes <b.guenes@live.com>
2022-05-12 13:43:02 +0200

    updated Homepage and workout as URL

--- app.py
+++ app.py
@@ -1,47 +1,56 @@
-from flask import Flask, render_template, request, send_from_directory, send_file
+from flask import Flask, render_template, request, send_from_directory, send_file, redirect, url_for, session
 from SpeedCoach import SpeedCoach
-import csv
 import numpy as np
 import os
 import io
 
+class DataBase(object):
+    x = None
 
+
+data = DataBase()
 
 app = Flask(__name__)
 
+key = os.urandom(16)
+app.config.update(SECRET_KEY=key)
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def homepage():
+    if request.method == "POST":
+
+        varW = request.form["workout"]
+        f = request.files['file']
+        data.x = np.char.split(np.asarray(f.read().decode("utf-8").split("\n")), ",")
+
+        return redirect(url_for("upload", var=varW))
+
     return render_template("home.html")
 
-@app.route("/uploader", methods = ['GET', 'POST'])
-def upload():
-    if request.method == "POST":
-        f = request.files['file']
-        mydata = np.char.split(np.asarray(f.read().decode("utf-8").split("\n")), ",")
+@app.route("/<var>")
+def upload(var):
 
-        workout = request.form["workout"]
+    mydata = data.x
 
-        pdfpath, file = SpeedCoach(mydata, workout)
+    workout = var
 
-        workingdir = os.path.abspath(os.getcwd())
-        pdfpath = workingdir+"/"+"data"+"/"+workout+"/"
-        print(pdfpath)
+    pdfpath, file = SpeedCoach(mydata, workout)
 
-        #return send_from_directory(pdfpath, file), os.remove(pdfpath+file)
+    workingdir = os.path.abspath(os.getcwd())
+    pdfpath = workingdir+"/"+"data"+"/"+workout+"/"
 
-        file_path = pdfpath+file
+    file_path = pdfpath+file
 
-        return_data = io.BytesIO()
-        with open(file_path, 'rb') as fo:
-            return_data.write(fo.read())
-            # (after writing, cursor will be at last byte, so move it to start)
-        return_data.seek(0)
+    return_data = io.BytesIO()
+    with open(file_path, 'rb') as fo:
+        return_data.write(fo.read())
+        # (after writing, cursor will be at last byte, so move it to start)
+    return_data.seek(0)
 
-        os.remove(file_path)
-        os.rmdir(pdfpath)
+    os.remove(file_path)
+    os.rmdir(pdfpath)
 
-        return send_file(return_data, mimetype='application/pdf', attachment_filename='download_filename.pdf')
+    return send_file(return_data, mimetype='application/pdf', attachment_filename=workout+".pdf")
 
 
 if __name__ == "__main__":
